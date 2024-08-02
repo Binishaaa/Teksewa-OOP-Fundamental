4701bd00a1193395923bb117668e2eb39c957ba7
TEKSEWA\awale.binisha <binishawale@gmail.com>
2024-08-02 09:41:14 +0545

    get username

--- ConsoleAppForFundamental/Program.cs
+++ ConsoleAppForFundamental/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// Console.WriteLine("Hello, World!");
+
+// Console.ReadLine();
+
+// string userInput =  Console.ReadLine();
+// Console.WriteLine($"Your entered  {userInput.Length} characters");
+
+//Get user name and convert it to uppercase
+
+Console.WriteLine("Enter your name");
+string userName = Console.ReadLine();
+Console.WriteLine($"your name is {userName.ToUpper()}");
